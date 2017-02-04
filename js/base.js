61e7fcac44cba3a7cd2e8fd18cbba01495e766f0
billy <billy_108@163.com>
2017-02-04 23:22:11 +0800

    update

--- js/base.js
+++ js/base.js
@@ -9,6 +9,24 @@ app.config(function ($routeProvider) {
 });
 
 $("#btn_addOrderList").click(function () {
-  console.log("11111111111111");
+  $("#container").show();
+});
+
+$("#addList").click(function () {
+  var txt = $("#text_addOrderList").val();
+  //内容不为空，异步提交内容
+  if(txt.length != 0){
+    $.post(
+      "./data/addOrderList.php",
+      txt,
+      function (data) {
+        console.log(data);
+        //隐藏addOrderList界面
+        $("#container").hide();
+      }
+    , "json");
+  }else{
+    confirm("请填写内容！")
+  }
 });
 
