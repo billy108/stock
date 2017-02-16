23ebe69547acdce2dda10ede05730d9573c08d96
billy <billy_108@163.com>
2017-02-17 00:03:15 +0800

    addorder done

--- data/addOrderList.php
+++ data/addOrderList.php
@@ -7,9 +7,16 @@
 
     //连接数据库
     $conn = mysqli_connect("127.0.0.1", "root", "", "stock");
-
+    //每次提交新订单时，都要先删除临时表中的数据
+    $sql = "TRUNCATE temp_orderList";
+    mysqli_query($conn, $sql);
+    //遍历新订单
     foreach($data as $item){
-        $sql = "insert temp_orderList values ('$item[0]','$item[1]','".trim($item[2])."',$item[3],$item[4])";
+        //把新订单添加进临时表中
+        $sql = "insert into temp_orderList values ('$item[0]','$item[1]','".trim($item[2])."',$item[3],$item[4],'')";
+        mysqli_query($conn, $sql);
+        //把新订单添加进订单总表中
+        $sql = "insert into orderList values ('$item[0]','$item[1]','".trim($item[2])."',$item[3],$item[4],'')";
         mysqli_query($conn, $sql);
     };
 ?>
